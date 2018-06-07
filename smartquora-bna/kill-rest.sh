4ce58bb4518e74bdcdd02e7897f026c0ee1586f9
Srini Karlekar <skarlekar@yahoo.com>
2018-06-07 14:07:51 -0400

    Update kill-rest.sh

--- smartquora-bna/kill-rest.sh
+++ smartquora-bna/kill-rest.sh
@@ -1,3 +1,3 @@
-PID=`lsof -i :3000 | cut -c 8-13 | grep -v "PID" | tr -d '[:space:]'`
+PID=`lsof -i :3000 | cut -c 8-13 | grep -v "PID" | head -n 1 | tr -d '[:space:]'`
 [ -z "$PID" ] && echo "Composer REST Server not running on port 3000" || echo "Killing $PID"
 [ -z "$PID" ] && echo "Nothing to kill!" || kill $PID
